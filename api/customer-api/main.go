d8a8df1d66ebb532be52e3744536df4ba47f10ca
cuongtop4598 <cuongtop4598@gmail.com>
2021-08-28 07:40:02 +0700

    add customer api

---
+++ api/customer-api/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// create grpc client connect
+	// clientConn, err := grpc.Dial(":2224", grpc.WithInsecure())
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// //Singleton
+	// customerClient := pb.NewCustormerClient(clientConn)
+
+}
